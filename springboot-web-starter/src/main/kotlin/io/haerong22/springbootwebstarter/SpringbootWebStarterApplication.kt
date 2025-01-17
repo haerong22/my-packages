2a364f1a3a5f71e1523f1b5507b4ec786510c278
haerong22 <haerong22@naver.com>
2025-01-17 22:07:20 +0900

    web: init

---
+++ springboot-web-starter/src/main/kotlin/io/haerong22/springbootwebstarter/SpringbootWebStarterApplication.kt
@@ -0,0 +1,11 @@
+package io.haerong22.springbootwebstarter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootWebStarterApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringbootWebStarterApplication>(*args)
+}
---
+++ springboot-web-starter/src/test/kotlin/io/haerong22/springbootwebstarter/SpringbootWebStarterApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.haerong22.springbootwebstarter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootWebStarterApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
